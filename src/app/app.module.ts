4b880080818ff927fdd2b7a103e420bba3d94fe6
Tian Dominguez <tiandominguez3@gmail.com>
2019-02-26 16:49:26 -0500

    Basic Additions
    Added some basic components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
