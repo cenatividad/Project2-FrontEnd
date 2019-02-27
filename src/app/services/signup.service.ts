6541ba4f6266584382fe27a8e9e86bc50d6eda5f
Tian Dominguez <tiandominguez3@gmail.com>
2019-02-27 12:26:42 -0500

    Login/Signup button disabling
    Added functionality to disable the login/signup buttons unless the entered input is correct.
    
    At this point "correct" means not null or empty.

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -20,4 +20,54 @@ export class SignupService {
   checkPasswords(password: string, passwordVerification: string): boolean{
     return password === passwordVerification;
   }
+
+
+  // Checks username for not being null/undefined or empty
+  usernameOK(username: string): boolean{
+    if(username && username !== ''){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  // Checks password for not being null/undefined or empty
+  passwordOK(password: string, passwordVerification: string): boolean{
+    if(password && password !== '' && this.checkPasswords(password, passwordVerification)){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  // Checks password for not being null/undefined or empty
+  emailOK(email: string): boolean{
+    email = email || '';
+
+    if(email !== ''){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  // Checks password for not being null/undefined or empty
+  nameOK(firstName: string, lastName: string): boolean{
+    firstName = firstName || '';
+    lastName = lastName || '';
+
+    if(firstName !== '' && lastName != ''){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  // Verifies that the passed inputs are correct in some way.
+  checkInput(username: string, password: string, passwordVerification: string, email: string,
+    firstName: string, lastName: string): boolean{
+      return(this.usernameOK(username) && this.passwordOK(password, passwordVerification) &&
+             this.emailOK(email) && this.nameOK(firstName, lastName));
+    
+  }
 }
