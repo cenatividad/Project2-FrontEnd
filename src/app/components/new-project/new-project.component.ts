8dcab4703232d14656641afa862b1471aee0be6e
BjornPrime <bjorn291@gmail.com>
2019-02-28 18:08:26 -0500

    midway through front-end

--- src/app/components/new-project/new-project.component.ts
+++ src/app/components/new-project/new-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from 'src/app/services/project.service';
+import { SessionService } from 'src/app/services/sessions.service';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'app-new-project',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProjectComponent implements OnInit {
 
-  constructor() { }
+  projectName = '';
+  description = '';
+  user = this.sessionService.getActiveUser();
+
+  constructor(private projectService: ProjectService, private sessionService: SessionService) { }
 
   ngOnInit() {
+
   }
 
+  newProject() {
+    let project: Project;
+    project.projectName = this.projectName;
+    project.description = this.description;
+    project.projectUsers.push(this.user);
+    this.user.projects.push(project);
+    this.projectService.createProject();
+  }
 }
