943c3f5d524c1aa05c4c03f15d6d5330c370a751
Chris Natividad <cenatividad91@gmail.com>
2019-03-02 15:20:23 -0500

    Added activeUserProjects array to session service.
    SessionService maintains list of the active user's projects.
    
    SessionService also has wrapper method to fetch the active user's
    projects via the UserService getUserProjects() method.
    
    Active user projects array is fetched following successful login.

--- src/app/services/sessions.service.ts
+++ src/app/services/sessions.service.ts
@@ -2,24 +2,46 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { NavigationService } from './navigation.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Project } from '../models/project';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
   private activeUser: User;
+  private activeUserProjects: Array<Project> = [];
 
-  constructor(private navigationService: NavigationService, private cookieService: CookieService) { }
+  constructor(private navigationService: NavigationService, private cookieService: CookieService, private userService: UserService) { }
 
   // Checks for a currently logged in user, logs them out if necessary, and assigns a new user.
   setActiveUser(user: User){
     //TODO rest of the logic
     this.activeUser = user;
+    this.fetchActiveUserProjects();
+  }
+
+  fetchActiveUserProjects() {
+    if (!this.activeUser || !this.activeUser.id) {
+      this.activeUserProjects = [];
+      return;
+    }
+
+    this.userService.getUserProjects(this.activeUser.id).subscribe((payload) => {
+      console.log('SessionService: getUserProjects success. ' + payload);
+      this.activeUserProjects = payload;
+    }, (error) => {
+      console.log('SessionService: error' + error);
+    });
   }
 
   // basic get for active user
-  getActiveUser(): User{
+  getActiveUser(): User {
     return this.activeUser;
+  }
+
+  getActiveUserProjects() {
+    return this.activeUserProjects;
   }
 
   // nullifies the current activeUser to prevent potential conflict
