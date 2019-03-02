d0d707e15176cdd1835f16242e724d4ac36ac172
BjornPrime <bjorn291@gmail.com>
2019-03-02 17:57:54 -0500

    project creators now own projects

--- src/app/components/new-project/new-project.component.ts
+++ src/app/components/new-project/new-project.component.ts
@@ -4,6 +4,7 @@ import { SessionService } from 'src/app/services/sessions.service';
 import { Project } from 'src/app/models/project';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { InvitationService } from 'src/app/services/invitation.service';
 
 @Component({
   selector: 'app-new-project',
@@ -22,7 +23,8 @@ export class NewProjectComponent implements OnInit {
   constructor(private projectService: ProjectService,
               private sessionService: SessionService,
               private router: Router,
-              private cookieService: CookieService) { }
+              private cookieService: CookieService,
+              private invitationService: InvitationService) { }
 
   ngOnInit() {
 
@@ -36,6 +38,8 @@ export class NewProjectComponent implements OnInit {
     this.projectService.createProject(this.project).subscribe( (payload: Project) => {
       console.log(payload);
       this.cookieService.set('projectID', payload.projectID.toString());
+      let credentials = { 'email' : this.user.email, 'projectID' : payload.projectID.toString()};
+      this.invitationService.addUserToProject(credentials).subscribe( (payload) => {});
       this.router.navigateByUrl(`/main/project`);
     }, (err) => {
       console.log(err);
