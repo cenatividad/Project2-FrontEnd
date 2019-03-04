480da563d9c1d5a3d3323079090e4083581e5a48
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2019-03-04 13:27:15 -0500

    something

--- src/app/components/view-invitations/view-invitations.component.ts
+++ src/app/components/view-invitations/view-invitations.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-view-invitations',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-invitations.component.css']
 })
 export class ViewInvitationsComponent implements OnInit {
+  invitations: Array<any>;
+  approveDeny = true;
 
-  constructor() { }
+  constructor(private cookieService: CookieService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  processInvitation(i: number, appDen: boolean) {
+    this.invitations[i];
   }
 
 }
