2b493a388d35709595b13b1ad087a97fbef2f344
Chris Natividad <cenatividad91@gmail.com>
2019-03-05 19:47:06 -0500

    Update story status via drag and drop
    Implemented drag and drop functionality to ProjectComponent
    in conjunction with StoryListComponents.
    
    Stories can be dragged and dropped across components.
    
    Story ordering in a list currently has no semantic meaning.

--- src/app/components/story-list/story-list.component.ts
+++ src/app/components/story-list/story-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Story } from 'src/app/models/story';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { StoryService } from 'src/app/services/story.service';
 
 @Component({
   selector: 'app-story-list',
@@ -8,10 +10,35 @@ import { Story } from 'src/app/models/story';
 })
 export class StoryListComponent implements OnInit {
   @Input() stories: Array<Story>;
+  @Input() dropLists: Array<string>;
+  @Input() status: string;
 
-  constructor() { }
+  constructor(private storyService: StoryService) { }
 
   ngOnInit() {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log('StoryListComponent: status = ', event, this.status);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+      // get story
+      const story = this.stories[event.currentIndex];
+      // assign new story status
+      story.status = this.status;
+      console.log('StoryListComponent: ', story);
+      // request backend to update data
+      this.storyService.updateStory(story).subscribe((payload) => {
+        console.log('StoryLstComponent: Story Updated');
+      }, (error) => {
+        console.log('StoryLstComponent: Story NOT Updated');
+      });
+    }
   }
 
 }
