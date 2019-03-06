6ea5268171075efffa3c6c395bc717552b3bc570
Chris Natividad <cenatividad91@gmail.com>
2019-03-06 17:04:33 -0500

    Fixed environment.prod.ts and added script to initilize base href

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseHref: '/scrum-dojo/',
+  baseHref: '/dojo-scrum/',
   APIbase: 'http://ec2-54-219-178-207.us-west-1.compute.amazonaws.com:8081/'
 };
